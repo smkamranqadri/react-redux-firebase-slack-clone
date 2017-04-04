eed26437424e922a7ef4ff1594db7a127438b383
Muhammad Kamran <smkamranqadri@yahoo.com>
2017-04-05 02:24:12 +0500

    production build and fixed login according to new lib.

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,17 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { firebaseConnect } from 'react-redux-firebase';
 
 import './login.css';
 
 import { loginWithGoogle } from './login.actions';
 
 class LoginContainer extends Component {
+  login() {
+    this.props.firebase.login({ provider: 'google', type: 'popup' });
+  }
   render() {
     return (
       <div className="center">
         <h1 className="text-center">Login to get started...</h1>
-        <img src={process.env.PUBLIC_URL + "/google_plus_icon.png"} width="200" height="200" alt="Google Login" onClick={this.props.loginWithGoogle}/>
+        <img src={process.env.PUBLIC_URL + "/google_plus_icon.png"} width="200" height="200" alt="Google Login" onClick={() => this.login()}/>
       </div>
     );
   }
@@ -31,7 +35,9 @@ LoginContainer.propTypes = {
 
 LoginContainer.contextTypes = { };
 
+const wrappedLogin = firebaseConnect()(LoginContainer);
+
 export let Login = connect(
   mapStateToProps,
   mapDispatchToProps
-)(LoginContainer);
+)(wrappedLogin);
